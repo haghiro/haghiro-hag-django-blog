eae89791213acf881ce8e4759718033ea6d8896b
haghiro <nauticatube@yahoo.co.jp>
2020-10-06 08:59:03 +0900

    aws設定2

--- myproject/settings.py
+++ myproject/settings.py
@@ -141,7 +141,7 @@ AWS_S3_OBJECT_PARAMETERS = {
 }
 
 
-AWS_LOCATION = '/media/'
+AWS_LOCATION = 'media'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
