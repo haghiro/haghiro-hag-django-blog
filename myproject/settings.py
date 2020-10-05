7fa6bbe69152579f4eca89475f45e15babb6421c
haghiro <nauticatube@yahoo.co.jp>
2020-10-06 08:49:05 +0900

    aws設定

--- myproject/settings.py
+++ myproject/settings.py
@@ -133,6 +133,14 @@ LOGIN_URL = 'myapp:login'
 LOGIN_REDIRECT_URL ='myapp:index'
 
 # S3のための記述
+
+AWS_STORAGE_BUCKET_NAME = 'hag-django-blog'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400', #１日はそのキャッシュを使用
+}
+
+
 AWS_LOCATION = '/media/'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
